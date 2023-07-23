bba36f34b975560b8bec091d7c1e86ac536c79ea
LSY <2028238109@qq.com>
2023-07-23 16:43:57 +0800

    Change variable name.

--- engineer_middleware/include/engineer_middleware/motion.h
+++ engineer_middleware/include/engineer_middleware/motion.h
@@ -551,8 +551,8 @@ public:
   ChassisMotion(XmlRpc::XmlRpcValue& motion, ChassisInterface& interface)
     : MotionBase<ChassisInterface>(motion, interface)
   {
-    chassis_tolerance_position_ = xmlRpcGetDouble(motion, "chassis_tolerance_position_", 0.01);
-    chassis_tolerance_angular_ = xmlRpcGetDouble(motion, "chassis_tolerance_angular_", 0.01);
+    chassis_tolerance_position_ = xmlRpcGetDouble(motion, "chassis_tolerance_position", 0.01);
+    chassis_tolerance_angular_ = xmlRpcGetDouble(motion, "chassis_tolerance_angular", 0.01);
     if (motion.hasMember("frame"))
       target_.header.frame_id = std::string(motion["frame"]);
     if (motion.hasMember("position"))
